9320915455cd93ae9d931c7e1d206e17548102de
MaggyProtasio <brend.protasio@edu.cobianchi.it>
2023-01-17 12:08:10 +0100

    -add println maggy commit2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ciao MAA");
         System.out.println("maggy potato");
+        System.out.println("maggy potat2222o");
     }
 }
