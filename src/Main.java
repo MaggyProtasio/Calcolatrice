2e338646196819b8d587ccbf1e76b844a9a52ae1
Nastya <anastasiya.kozemko@edu.cobianchi.it>
2023-02-07 11:01:36 +0100

    reset progetto in calcolatrice

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ciao MAA");
-        System.out.println("maggy potato");
-        System.out.println("maggy potat2222o");
-        System.out.println("Commento ------------------");
+
     }
 }
